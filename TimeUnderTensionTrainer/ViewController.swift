0eb2f3a4f97d1d9680c6528a0fda9dd232869050
Charlie Griffin <charliegriffin@email.arizona.edu>
2018-05-26 18:23:19 -0500

    Initial Commit

---
+++ TimeUnderTensionTrainer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeUnderTensionTrainer
+//
+//  Created by Charles Griffin on 5/26/18.
+//  Copyright © 2018 Charles Griffin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
